8295226e2166f3d9ef07d866f7d755b050169126
Thai Duong Vu <vu.t.duong01@gmail.com>
2020-08-13 17:58:23 -0400

    Refactor game won events

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ black = (34, 40, 49)
 start_text = text_font.render("Press Space to start", True, white)
 
 
-def show_score(player):
+# Display player scores
+def display_score(player):
     player_score_text = number_font.render(
         str(player.get_score()), True, white)
 
@@ -45,7 +46,8 @@ def show_score(player):
                           (int)(screen_width / 4 * 3 - text_size / 4), 20])
 
 
-def player_win_actions(player, ball):
+# What to do when a player wins
+def player_win(player, ball):
     if player.get_id() == 1:
         player_text = text_font.render("Player 1 wins", True, white)
     else:
@@ -57,16 +59,15 @@ def player_win_actions(player, ball):
     ball.speed_y = 0
 
 
-def player_win(player1, player2, ball):
+# Return game winner when either player reaches max score
+def winner(player1, player2, ball):
     if player1.get_score() >= max_score and player1.get_score() >= player2.get_score() + 2:
-        player_win_actions(player1, ball)
-        return True
+        return player1
 
     elif player2.get_score() >= max_score and player2.get_score() >= player1.get_score() + 2:
-        player_win_actions(player2, ball)
-        return True
+        return player2
 
-    return False
+    return None
 
 
 def quit_game():
@@ -79,21 +80,23 @@ def main_loop():
     game_exit = False
     game_over = False
 
-    separator = Separator(white, screen_width)
-    ball = Ball(Direction.No, white, screen_width, screen_height)
-
     player1 = Player(1, white, screen_width, screen_height)
     player2 = Player(2, white, screen_width, screen_height)
+
+    ball = Ball(Direction.No, white, screen_width, screen_height)
+    separator = Separator(white, screen_width)
 
     while not game_exit:
         for event in pygame.event.get():
             if event.type == QUIT:
                 game_exit = True
 
+            # Escape game when escape key is pressed
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     game_exit = True
 
+                # Move players with arrow keys and WASD
                 if start:
                     if event.key == K_w:
                         player1.move_up()
@@ -105,6 +108,7 @@ def main_loop():
                     if event.key == K_DOWN:
                         player2.move_down()
 
+                # Press Space to start game
                 if event.key == K_SPACE:
                     if not start:
                         start = True
@@ -113,14 +117,17 @@ def main_loop():
                     if game_over:
                         main_loop()
 
+            # Stop moving player if arrows or WASD is up
             if event.type == KEYUP:
                 if event.key == K_w or event.key == K_s:
                     player1.stop()
                 if event.key == K_UP or event.key == K_DOWN:
                     player2.stop()
 
+        # Fill black background
         game_display.fill(black)
 
+        # Render game objects and UI
         if start:
             ball.draw(game_display)
         separator.draw(game_display, screen_height)
@@ -131,14 +138,12 @@ def main_loop():
         if not start and not game_over:
             game_display.blit(start_text, [240, 200])
 
-        show_score(player1)
-        show_score(player2)
+        display_score(player1)
+        display_score(player2)
 
+        # Check collisions
         if not game_over:
-            if ball.check_collision_player(player1):
-                ball.change_direction_horizontal()
-
-            if ball.check_collision_player(player2):
+            if ball.check_collision_player(player1) or ball.check_collision_player(player2):
                 ball.change_direction_horizontal()
 
             if ball.check_collision_wall(screen_height):
@@ -146,15 +151,17 @@ def main_loop():
 
             if ball.check_score(player1, player2, screen_width, screen_height) == player1:
                 ball.__init__(Direction.Left, white,
-                                     screen_width, screen_height)
+                              screen_width, screen_height)
                 player1.add_score()
-
-            if ball.check_score(player1, player2, screen_width, screen_height) == player2:
+            elif ball.check_score(player1, player2, screen_width, screen_height) == player2:
                 ball.__init__(Direction.Right, white,
-                                     screen_width, screen_height)
+                              screen_width, screen_height)
                 player2.add_score()
 
-        if player_win(player1, player2, ball):
+        # Handle game winning
+        if winner(player1, player2, ball) != None:
+            player_win(winner(player1, player2, ball), ball)
+
             start = False
             game_over = True
 
