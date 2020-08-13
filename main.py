5423f7af57a816dfd6fd4349d7d4acc56f432631
Thai Duong Vu <vu.t.duong01@gmail.com>
2020-08-13 15:32:53 -0400

    Update object and variable names

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def show_score(player):
                           (int)(screen_width / 4 * 3 - text_size / 4), 20])
 
 
-def player_win_actions(player, ball_object):
+def player_win_actions(player, ball):
     if player.get_id() == 1:
         player_text = text_font.render("Player 1 wins", True, white)
     else:
@@ -53,17 +53,17 @@ def player_win_actions(player, ball_object):
 
     game_display.blit(player_text, [300, 200])
 
-    ball_object.speed_x = 0
-    ball_object.speed_y = 0
+    ball.speed_x = 0
+    ball.speed_y = 0
 
 
-def player_win(player1, player2, ball_object):
+def player_win(player1, player2, ball):
     if player1.get_score() >= max_score and player1.get_score() >= player2.get_score() + 2:
-        player_win_actions(player1, ball_object)
+        player_win_actions(player1, ball)
         return True
 
     elif player2.get_score() >= max_score and player2.get_score() >= player1.get_score() + 2:
-        player_win_actions(player2, ball_object)
+        player_win_actions(player2, ball)
         return True
 
     return False
@@ -80,7 +80,7 @@ def main_loop():
     game_over = False
 
     separator = Separator(white, screen_width)
-    ball_object = Ball(Direction.No, white, screen_width, screen_height)
+    ball = Ball(Direction.No, white, screen_width, screen_height)
 
     player1 = Player(1, white, screen_width, screen_height)
     player2 = Player(2, white, screen_width, screen_height)
@@ -108,7 +108,7 @@ def main_loop():
                 if event.key == K_SPACE:
                     if not start:
                         start = True
-                        ball_object.__init__(
+                        ball.__init__(
                             Direction.Left, white, screen_width, screen_height)
                     if game_over:
                         main_loop()
@@ -122,7 +122,7 @@ def main_loop():
         game_display.fill(black)
 
         if start:
-            ball_object.draw(game_display)
+            ball.draw(game_display)
         separator.draw(game_display, screen_height)
 
         player1.draw(game_display, screen_height)
@@ -135,26 +135,26 @@ def main_loop():
         show_score(player2)
 
         if not game_over:
-            if ball_object.check_collision_player(player1):
-                ball_object.change_direction_horizontal()
+            if ball.check_collision_player(player1):
+                ball.change_direction_horizontal()
 
-            if ball_object.check_collision_player(player2):
-                ball_object.change_direction_horizontal()
+            if ball.check_collision_player(player2):
+                ball.change_direction_horizontal()
 
-            if ball_object.check_collision_wall(screen_height):
-                ball_object.change_direction_vertical()
+            if ball.check_collision_wall(screen_height):
+                ball.change_direction_vertical()
 
-            if ball_object.check_score(player1, player2, screen_width, screen_height) == player1:
-                ball_object.__init__(Direction.Left, white,
+            if ball.check_score(player1, player2, screen_width, screen_height) == player1:
+                ball.__init__(Direction.Left, white,
                                      screen_width, screen_height)
                 player1.add_score()
 
-            if ball_object.check_score(player1, player2, screen_width, screen_height) == player2:
-                ball_object.__init__(Direction.Right, white,
+            if ball.check_score(player1, player2, screen_width, screen_height) == player2:
+                ball.__init__(Direction.Right, white,
                                      screen_width, screen_height)
                 player2.add_score()
 
-        if player_win(player1, player2, ball_object):
+        if player_win(player1, player2, ball):
             start = False
             game_over = True
 
